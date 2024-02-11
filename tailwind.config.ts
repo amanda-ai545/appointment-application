7edec1390cf01e1cc87dccd82ead94d7574863ae
Amanda Evallo <amandadianeevallo@gmail.com>
2024-02-11 17:42:05 +0800

    feat: setup tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "white-300": `#FFFFFF0D`,
+        "white-200": `#FFFFFF33`,
+        "white-100": `#FFFFFF80`,
+        "dark-100": `#333234`,
+        dark: `#1C1C1E`,
+        "gray-100": `#9D9C9C`,
+        gray: `#D8D3CC`,
+        "orange-300": `#e6590d`,
+        "orange-200": `#FFE0CE`,
+        "orange-100": `#F25D23`,
+        orange: `#FF630B`,
+      },
+      screens: {
+        desktop: {min: "1920px"},
+      },
+    },
   },
   plugins: [],
 };
